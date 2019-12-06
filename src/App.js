d5fa59b21024362b8f1921d72d54e6e43d512f09
Marvin Matovu <marvinmatovu@gmail.com>
2019-12-06 20:55:04 +0000

    initial commit - setup bootstrap

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+class App extends React.Component{
+  render() {
+    return (
+      <div>
+        <h1>COMPONENTS</h1>
+      </div>
+      
+    )
+  }
+}
+
+export default App;

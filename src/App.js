f6180e9e780854368241f919de582c9ac2d19439
Marvin Matovu <marvinmatovu@gmail.com>
2019-12-06 21:11:55 +0000

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Signup from './components/Signup';
 
 
 class App extends React.Component{
   render() {
     return (
-      <div>
-        <h1>COMPONENTS</h1>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/signup">Signup</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+        </Switch>
+        </div>
+      </Router>
       
     )
   }
